c6c96806523ecabd02b8ed622b0b58dd6d279464
Jack Bresnahan <jpbresnah@coastal.edu>
2021-09-13 22:23:15 -0400

    Fixed ID so it is given not decided by the user

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace GameScheduler.Models {
     public class User {
         public string Name {get;set;}
-        public int Id {get;set;}
+        public int Id {get;}
         public string Password {get;set;}
         public List<Game> FavoriteGames {get;set;}
         public string Bio {get;set;}
