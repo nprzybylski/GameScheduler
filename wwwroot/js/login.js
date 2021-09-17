1b001fa559d9d230a7229965939b93d8822608bc
Jack Bresnahan <jpbresnah@coastal.edu>
2021-09-16 23:14:45 -0400

    added UI for login -- login and registration

---
+++ wwwroot/js/login.js
@@ -0,0 +1,57 @@
+const uri = 'GameScheduler';
+let user = [];
+
+/*
+function addItem() {
+  const addTitleTextbox = document.getElementById('add-title');
+  const addDescriptionTextbox = document.getElementById('add-description');
+  const addGenreTextBox = document.getElementById('add-genre');
+
+  const item = {
+    title: addTitleTextbox.value.trim(),
+    description: addDescriptionTextbox.value.trim(),
+    genre: addGenreTextBox.value.trim()
+  };
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+    .then(response => response.json())
+    .then(() => {
+      getItems();
+      addTitleTextbox.value = '';
+      addDescriptionTextbox.value = '';
+      addGenreTextBox.value = '';
+    })
+    .catch(error => console.error('Unable to add item.', error));
+}
+*/
+/*
+function deleteItem(title) {
+  fetch(`${uri}/${title}`, {
+    method: 'DELETE'
+  })
+  .then(() => getItems())
+  .catch(error => console.error('Unable to delete item.', error));
+}
+*/
+
+//NEED TO IMPLEMENT
+
+//REDIRECT TO REGISTRATION
+
+
+//ADD NEW USER
+
+
+
+
+
+
+//LOGIN
+
