3edaba468f3e49e640924ed5674f867209f3c022
Nixtertec <ndkukolj@coastal.edu>
2021-10-08 16:22:27 -0400

    Integrating Front-End functionality for GetGameEventsWithDate

--- wwwroot/js/gameevent.js
+++ wwwroot/js/gameevent.js
@@ -8,6 +8,19 @@ function getItems() {
     .catch(error => console.error('Unable to get items.', error));
 }
 
+// Fetch events by date
+
+function getItemsByDate() {
+  //fetch(`${uri}/${date}`)
+    //2021-09-27
+    fetch(`${uri}/date/2021-09-27`)
+
+    .then(response => response.json())
+    .then(data => _displayItemsByDate(data, date))
+    .catch(error => console.error('Unable to get items.', error));
+}
+
+//--------------------------------------------------------------------------------
 function addItem() {
   const addNameTextbox = document.getElementById('add-name');
 
@@ -142,4 +155,74 @@ function _displayItems(data) {
   });
 
   eventTable = data;
+}
+
+
+// Display items by date
+
+function _displayItemsByDate(data){
+  
+  const tBody = document.getElementById('eventByDateTable');
+  tBody.innerHTML = '';
+
+  _displayCount(data.length);
+
+  const button = document.createElement('button');
+
+  data.forEach(item => {
+   
+   
+    let isCompleteCheckbox = document.createElement('input');
+    isCompleteCheckbox.type = 'checkbox';
+    isCompleteCheckbox.disabled = true;
+    isCompleteCheckbox.checked = item.isComplete;
+
+    let editButton = button.cloneNode(false);
+    editButton.innerText = 'Edit';
+    editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+
+    let deleteButton = button.cloneNode(false);
+    deleteButton.innerText = 'Delete';
+    deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+
+    let tr = tBody.insertRow();
+    
+    let td1 = tr.insertCell(0);
+    let textNode1 = document.createTextNode(item.id)
+    td1.appendChild(textNode1);
+
+    
+
+    let td2 = tr.insertCell(1);
+    let textNode2 = document.createTextNode(item.title);
+    td2.appendChild(textNode2);
+
+
+    let td3 = tr.insertCell(2);
+    let textNode3 = document.createTextNode(item.users);
+    td3.appendChild(textNode3);
+
+
+    let td4 = tr.insertCell(3);
+    let textNode4 = document.createTextNode(item.gameTitle);
+    td4.appendChild(textNode4);
+
+    let td5 = tr.insertCell(4);
+    let textNode5 = document.createTextNode(item.capacity);
+    td5.appendChild(textNode5);
+
+    let td6 = tr.insertCell(5);
+    let textNode6 = document.createTextNode(item.time);
+    td6.appendChild(textNode6);
+
+    let td7 = tr.insertCell(6);
+    let textNode7 = document.createTextNode(item.description);
+    td7.appendChild(textNode7);
+
+  });
+
+  eventByDateTable = data;
+
+
+
 }
